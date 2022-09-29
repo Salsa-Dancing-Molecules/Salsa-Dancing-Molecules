86f37f96a53c4417e84ac59312a900e78ad55b6d
Andreas Kempe <kempe@lysator.liu.se>
2022-09-29 20:16:12 +0200

    Add basic Python package structure to the project
    This structures the project as a Python package with an installation
    configuration generating salsa-dancing-molecules as a command-line
    utility.
    
    When salsa-dancing-molecules is run, it invokes the main function found
    in salsa_dancing_molecules/cli/main.py which is the program's
    entry-point.

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""This is a setuptools based installation script."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Salsa-Dancing-Molecules",
+    version="0.0.1",
+    description="A molecular dynamics simulation software",
+    url="https://github.com/Salsa-Dancing-Molecules/Salsa-Dancing-Molecules",
+    classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3 :: Only",
+    ],
+    packages=find_packages(where="."),
+    python_requires=">=3.6",
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "salsa-dancing-molecules=salsa_dancing_molecules:main",
+        ],
+    },
+)

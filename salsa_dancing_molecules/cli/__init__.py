85361550ca7af545f37171b7ddd547b34555b57f
Andreas Kempe <kempe@lysator.liu.se>
2022-10-13 15:55:32 +0200

    Fix all lint issues
    Introduction of linting in the CI revealed some existing problems that
    need fixing.

--- salsa_dancing_molecules/cli/__init__.py
+++ salsa_dancing_molecules/cli/__init__.py
@@ -1,1 +1,7 @@
+"""Command line utility package.
+
+cli contains the main function that parses and runs the
+salsa-dancing-molecules MD program.
+"""
+
 from .main import main
--- salsa_dancing_molecules/cli/main.py
+++ salsa_dancing_molecules/cli/main.py
@@ -1,8 +1,10 @@
 """Module containing the main function of the simulation software."""
 
+
 def main():
     """Run the simulation software."""
     print("Implement some functionality!")
+
 
 if __name__ == '__main__':
     main()

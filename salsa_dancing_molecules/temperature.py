ea42f90c50c83a82564b8f83515f2962890768f0
edvva488 <edvva488@student.liu.se>
2022-10-14 10:29:39 +0200

    Fixed kinetic energy not being average kinetic energy.≈

--- salsa_dancing_molecules/temperature.py
+++ salsa_dancing_molecules/temperature.py
@@ -5,7 +5,7 @@ The function takes in an atoms object and returns the temperature.
 from ase import units
 
 def get_temperature(atoms):
-    return atoms.get_kinetic_energy()/(1.5*units.kB)
+    return (atoms.get_kinetic_energy()/len(atoms))/(1.5*units.kB)
 
 if __name__ == "__main__":
     get_temperature(atoms)	

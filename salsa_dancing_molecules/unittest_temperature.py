f33ac974499fb0c63d47c7d83c772c85620d5796
edvva488 <edvva488@student.liu.se>
2022-10-14 15:33:50 +0200

    Fixed syntax errors in unittest file.

--- salsa_dancing_molecules/unittest_temperature.py
+++ salsa_dancing_molecules/unittest_temperature.py
@@ -12,11 +12,11 @@ class TemperatureTest(unittest.TestCase):
 		atoms = FaceCenteredCubic(directions=[[1,0,0],[0,1,0],[0,0,1]], symbol='Cu', size=(3,3,3),pbc=True)
 		MaxwellBoltzmannDistribution(atoms, temperature_K=300)
 		
-		self.assertequal(get_temperature(atoms), 300)
+		self.assertEqual(get_temperature(atoms), 300)
 
 if __name__ =="__main__":
 	tests = [unittest.TestLoader().loadTestsFromTestCase(TemperatureTest)]
 	testsuite = unittest.TestSuite(tests)
-	results = unittest.TextTestRunner(verbosity=0).run(testsuite)
+	result = unittest.TextTestRunner(verbosity=0).run(testsuite)
 	sys.exit(not result.wasSuccessful())
 		

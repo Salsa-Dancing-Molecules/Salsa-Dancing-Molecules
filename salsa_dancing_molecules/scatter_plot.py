75f695842b6cdf7ec2b75488b5fc4dee1fdb8b11
Olle <ollny930@student.liu.se>
2022-11-07 16:09:47 +0100

    add ability to enter axis labels

--- salsa_dancing_molecules/scatter_plot.py
+++ salsa_dancing_molecules/scatter_plot.py
@@ -3,13 +3,17 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-def draw_scatter_plot(x_axis, y_axis, filename):
+def draw_scatter_plot(x_axis, y_axis, xlabel, ylabel, filename):
     """
     Draw a scatter plot from two given vectors.
+
+    Arguments in are: x-axis, y-axis, x-label, y-label, file name.
 
     File is saved as filename (which should include .png).
     """
     # Agg -> use non-interactive backend to save plot to file
     matplotlib.use('Agg')
     plt.scatter(x_axis, y_axis)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.savefig(filename)

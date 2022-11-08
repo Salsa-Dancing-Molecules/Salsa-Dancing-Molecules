a2d13562e94ce76717a0b957a20d71007ceb3068
Andreas Kempe <kempe@lysator.liu.se>
2022-11-08 14:52:24 +0100

    unit_test: test_scatter_plot: rewrite test on pytest format
    We're using pytest in the project, rewrite the test to be compatible.

--- salsa_dancing_molecules/unit_test/test_scatter_plot.py
+++ salsa_dancing_molecules/unit_test/test_scatter_plot.py
@@ -1,25 +1,14 @@
 """Unit test for scatter_plot.py."""
 from ..scatter_plot import draw_scatter_plot
-import unittest
 import pathlib as pl
 
 
-class PlotTests(unittest.TestCase):
-    """Class for testing creation of scatter plot file."""
+def test_scatter_plot():
+    """Creates simple scatter plot and checks if it is saved in a file."""
+    x_axis = [1, 2, 3, 4]
+    y_axis = [1, 4, 9, 16]
+    filename = 'test_plot.png'
 
-    def test_scatter_plot(self):
-        """Creates simple scatter plot and checks if it is saved in a file."""
-        x_axis = [1, 2, 3, 4]
-        y_axis = [1, 4, 9, 16]
-        filename = 'test_plot.png'
-
-        draw_scatter_plot(x_axis, y_axis, filename)
-        path = pl.Path(filename)
-        self.assertTrue(path.is_file())
-
-
-if __name__ == "__main__":
-    tests = [unittest.TestLoader().loadTestsFromTestCase(PlotTests)]
-    testsuite = unittest.TestSuite(tests)
-    result = unittest.TextTestRunner(verbosity=0).run(testsuite)
-    sys.exit(not result.wasSuccessful())
+    draw_scatter_plot(x_axis, y_axis, filename)
+    path = pl.Path(filename)
+    assert path.is_file()

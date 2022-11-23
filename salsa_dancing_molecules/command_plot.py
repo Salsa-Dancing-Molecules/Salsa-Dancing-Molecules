f6fa2f19ad7598fd5c45aa59fbb74e7534d192c4
Andreas Kempe <kempe@lysator.liu.se>
2022-11-23 15:58:00 +0100

    command_plot: don't use pandas for CSV
    Use the standard library CSV parser instead of pandas because of issues
    on FreeBSD.

--- salsa_dancing_molecules/command_plot.py
+++ salsa_dancing_molecules/command_plot.py
@@ -2,9 +2,9 @@
 from .variables import Variables
 from .scatter_plot import (draw_scatter_plot,
                            draw_four_scatter_plots)
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
-from pandas import *
 
 
 def data_plot(Q='-', filename='simulation_data.csv',
@@ -18,35 +18,49 @@ def data_plot(Q='-', filename='simulation_data.csv',
 
     Newfile: The name of the file the image will be saved to
     """
-    data = read_csv(filename)
-    time_step = data['Time (fs)']
+    time_step = []
+    kinetic = []
+    potential = []
+    pressure = []
+    temperature = []
+    heat_capacity = []
+
+    with open(filename, 'r') as csv_file:
+        data = csv.DictReader(csv_file)
+        next(data)
+        for row in data:
+            potential.append(float(row['Potential Energy (eV)']))
+            kinetic.append(float(row['Kinetic Energy (eV)']))
+            pressure.append(float(row['Pressure (Pa)']))
+            temperature.append(float(row['Temperature (K)']))
+            heat_capacity.append(float(
+                row['Specific Heat Capacity NVE (J/[K kg])']))
+            time_step.append(float(row['Time (fs)']))
 
     if Q != '-':
         if Q == 'Ekin':
-            draw_scatter_plot(time_step, data['Kinetic Energy (eV)'].tolist(),
+            draw_scatter_plot(time_step, kinetic,
                               '(fs)', '(eV)', newfile, show)
         elif Q == 'Epot':
-            draw_scatter_plot(time_step,
-                              data['Potential Energy (eV)'].tolist(),
+            draw_scatter_plot(time_step, potential,
                               '(fs))', '(eV)', newfile, show)
         elif Q == 'Pres':
-            draw_scatter_plot(time_step, data['Pressure (Pa)'].tolist(),
+            draw_scatter_plot(time_step, pressure,
                               '(fs)', '(Pa)', newfile, show)
         elif Q == 'Temp':
-            draw_scatter_plot(time_step, data['Temperature (K)'].tolist(),
+            draw_scatter_plot(time_step, temperature,
                               '(fs)', '(K)', newfile, show)
         elif Q == 'cnve':
-            draw_scatter_plot(time_step, data[
-                'Specific Heat Capacity NVE (J/[K kg])'].tolist(),
-                '(fs)', '( J/[K kg] )', newfile, show)
+            draw_scatter_plot(time_step, heat_capacity,
+                              '(fs)', '( J/[K kg] )', newfile, show)
         else:
             print('Unknown command, try agian. For help use -h or --help ')
     else:
         # converting column data to list
         draw_four_scatter_plots(time_step, [
-            data['Potential Energy (eV)'].tolist(),
-            data['Kinetic Energy (eV)'].tolist(),
-            data['Pressure (Pa)'].tolist(),
-            data['Temperature (K)'].tolist(),
-            data['Specific Heat Capacity NVE (J/[K kg])'].tolist()
+            potential,
+            kinetic,
+            pressure,
+            temperature,
+            heat_capacity,
         ], newfile, show)

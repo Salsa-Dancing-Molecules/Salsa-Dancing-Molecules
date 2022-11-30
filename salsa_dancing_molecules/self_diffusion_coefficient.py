9f4c25b3bcc15befe005341cda2f41bfe8c622c8
Kristoffer Gubberud Maras <krima334@student.liu.se>
2022-11-30 15:47:49 +0100

    Fixed self diffusion so that it now takes eqiulibrium time into consideration.
    co author Anton Skott

--- salsa_dancing_molecules/self_diffusion_coefficient.py
+++ salsa_dancing_molecules/self_diffusion_coefficient.py
@@ -14,29 +14,31 @@ def get_self_diffusion_coefficient(traj_file):
     returns:
         self_diffusion_coefficient - float
     """
-    MSD = get_msd(traj_file, reference="initial")
+    [MSD, MSD_average] = get_msd(traj_file, reference="initial")
     self_diffusion_coefficient = calculate_self_diffusion_coefficient(MSD)
 
     return self_diffusion_coefficient
 
 
-def calculate_self_diffusion_coefficient(MSD):
+def calculate_self_diffusion_coefficient(MSD, t0):
     """
     Take a list of mean square displacement for each timestep.
 
     arguments:
         MSD - list
+        t0 - integer
 
     returns:
         self_diffusion_coefficient - float
 
-    self_diffusion_coefficient is calculated by taking the last value of the
-    mean square displacement. Since MSD is calculated with the initial value as
+    self_diffusion_coefficient is calculated by comparing the slope between the
+    last element of mean square displacement and the element at the start of
+    the eqiulibrium. Since MSD is calculated with the initial value as
     reference the last value will give us the slope of MSD. This presupposes
     that the material being simulated is a liquid, which will give a linear
     slope.
     """
     t = len(MSD)
-    self_diffusion_coefficient = MSD[-1]/(6*t)
+    self_diffusion_coefficient = (MSD[-1] - MSD[t0])/(6*t)
 
     return self_diffusion_coefficient
--- salsa_dancing_molecules/unit_test/test_self_diffusion.py
+++ salsa_dancing_molecules/unit_test/test_self_diffusion.py
@@ -5,10 +5,11 @@ from ..self_diffusion_coefficient import calculate_self_diffusion_coefficient
 from unittest.mock import Mock
 
 MSD = [0, 7.5]
+t0 = 0
 
 
 def test_self_diffusion_coefficient():
     """Function for testing the self diffusion coefficient."""
-    result = calculate_self_diffusion_coefficient(MSD)
+    result = calculate_self_diffusion_coefficient(MSD, t0)
     test_success = 0.625
     assert result == pytest.approx(test_success, 0.0001)

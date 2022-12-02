2c7dcc1733ebdad2fcc4d34997645895fd75eb78
Kristoffer Gubberud Maras <krima334@student.liu.se>
2022-12-02 16:18:23 +0100

    Create a function for calculating the debye temperature.
    Co Author Anton Skott <antsk499@student.liu.se>

--- salsa_dancing_molecules/debye_temperature.py
+++ salsa_dancing_molecules/debye_temperature.py
@@ -1,13 +1,33 @@
 """Module for calculating the Debye temperature time average."""
 from ase import units
+from ase.io import read
 
 
-def get_debye_temperature(traj_file, temperature, specific_heat_capacity)
+def get_debye_temperature(traj_file, temperature, specific_heat_capacity):
+    """
+    Read trajectory file and call calculation function.
+
+    arguments:
+        traj_file               - trajectory file
+        temperature             - float
+        specific_heat_capacity  - float
+
+    return:
+        debye_temperature - float
+    """
+    configs = read(traj_file + "@0:1")  # Only one timestep is needed
+    debye_temperature = (calculate_debye_temperature(configs, temperature,
+                         specific_heat_capacity))
+
+    return debye_temperature
+
+
+def calculate_debye_temperature(configs, temperature, specific_heat_capacity):
     """
     Calculate the debye temperature.
 
     arguments:
-        traj_file               - trajectory file
+        configs                 - list
         temperature             - float
         specific_heat_capacity  - float
 
@@ -17,11 +37,8 @@ def get_debye_temperature(traj_file, temperature, specific_heat_capacity)
     This function assumes that the temperature is low. If temperature is high
     the results might not be accurate.
     """
-    configs = read(traj_file + "@0")
     N = len(configs[0])  # Number of atoms
-
-    #This is the simple function (maybe change this later)
-    debye_temperature = (temperature*((12*pi**4*N*units.kb)/(5*
-                        specific_heat_capacity))**(1/3))
+    debye_temperature = (temperature*((12*units.pi**4*N*units.kB)/(5 *
+                         specific_heat_capacity))**(1/3))
 
     return debye_temperature

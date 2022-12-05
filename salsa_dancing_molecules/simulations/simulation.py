acb3ff7fd63675e4f4a6c1bfbe272ded8a555f14
Edvard Valentin <edvva488@student.liu.se>
2022-12-05 15:20:43 +0100

    Fixed lint issues.

--- salsa_dancing_molecules/simulations/simulation.py
+++ salsa_dancing_molecules/simulations/simulation.py
@@ -1,4 +1,13 @@
+"""Module for running a simulation."""
+
+
 def run(sim_info, atoms):
+    """Run the simulation.
+
+    Args:
+        sim_info - dictionary with information on the simulation.
+        atoms - atoms object to be used.
+    """
     # TODO: implement real simulation
     f = open(sim_info["traj_output_path"], "w")
     f.write(sim_info["potential"])

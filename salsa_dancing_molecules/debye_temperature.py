d7232ba086200ac0f670540393d87760180120ec
Kristoffer Gubberud Maras <krima334@student.liu.se>
2022-12-12 15:43:34 +0100

    Create functionality for error messages for debye temperature and equilibrium.
    co Author: Anton Skott <antsk499@student.liu.se>

--- salsa_dancing_molecules/debye_temperature.py
+++ salsa_dancing_molecules/debye_temperature.py
@@ -16,13 +16,13 @@ def get_debye_temperature(traj_file, temperature, specific_heat_capacity):
         debye_temperature - float
     """
     configs = read(traj_file + "@0:1")  # Only one timestep is needed
-    debye_temperature = (calculate_debye_temperature(configs, temperature,
+    debye_temperature = (calculate_debye(configs, temperature,
                          specific_heat_capacity))
 
     return debye_temperature
 
 
-def calculate_debye_temperature(configs, temperature, specific_heat_capacity):
+def calculate_debye(configs, temperature, specific_heat_capacity):
     """
     Calculate the debye temperature.
 
@@ -38,7 +38,8 @@ def calculate_debye_temperature(configs, temperature, specific_heat_capacity):
     the results might not be accurate.
     """
     N = len(configs[0])  # Number of atoms
-    debye_temperature = (temperature*((12*units.pi**4*N*units.kB)/(5 *
+    debye_temperature = (temperature*((12*units.pi**4*N*units._k)/(5 *
                          specific_heat_capacity))**(1/3))
+    warning = 0.3 < temperature/debye_temperature
 
-    return debye_temperature
+    return debye_temperature, warning

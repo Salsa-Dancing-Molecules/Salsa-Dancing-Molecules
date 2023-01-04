30dca9eff21d0d3350f2d76e38a31900dbbc3043
Andreas Kempe <kempe@lysator.liu.se>
2023-01-04 18:41:08 +0100

    docs: generate module documentation
    Use sphinx-apidoc to generate module documentation from docstrtings in
    files.

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,6 @@ extensions = [
             'sphinx.ext.duration',
             'sphinx.ext.doctest',
             'sphinx.ext.autodoc',
-            'sphinx.ext.autosummary',
             'sphinx.ext.intersphinx',
 ]
 

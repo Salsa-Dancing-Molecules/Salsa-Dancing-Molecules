1b2e9e015b81909f9465e7e44b1aac0ff7d77b57
Andreas Kempe <kempe@lysator.liu.se>
2023-01-13 16:57:48 +0100

    Add data and graph script for Copper NVT analysis

---
+++ plot-generation/fcc_cu_heat_capacity_vs_scale.py
@@ -0,0 +1,21 @@
+import csv
+import matplotlib.pyplot as plt
+import math
+
+post_calc_info = []
+with open(f'fcc_cu_data/post_process_trimmed.csv') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        post_calc_info.append(row)
+
+
+plt.scatter([ float(x["volume"]) for x in post_calc_info],
+            [round(float(x["heat_capacity"]), 2) for x in post_calc_info], label='FCC Cu')
+
+
+plt.ylabel('specific heat capacity (J kg^-1 K^-1)')
+plt.xlabel('volume scale');
+plt.title('Specific heat capacity vs volume scale')
+plt.legend()
+
+plt.show()
